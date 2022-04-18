8b141a334aed122b2df91e4eecc10aa6e8b4400c
汪立超 <wanglichao.1121@163.com>
2022-04-19 00:26:28 +0800

    🤔 改善了文章底部的观感，更完诞生日日记

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -4,13 +4,20 @@ import { useRoute } from "vue-router"
 const route=useRoute()
 </script>
 <template>
-    <div class="post-body c-html-render">
-        <router-view/>
+    <div class="post-body">
+        <router-view class="c-html-render"/>
+        <div class="tail">没mò了liǎo</div>
     </div>
 </template>
-<style>
+<style scoped>
 .post-body{
     width: 90%;
     margin: auto;
+    padding-bottom: 20px;
+}
+.tail {
+    border-bottom: 5px rgb(64,70,75) solid;
+    text-align: right;
+    font-size: 50px;
 }
 </style>
